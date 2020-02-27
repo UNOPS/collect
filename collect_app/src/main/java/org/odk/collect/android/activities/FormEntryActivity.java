77ad481b1034af375bf99340b78565b22c628bfb
Callum Stott <callum@seadowg.com>
2020-02-27 11:52:52 +0000

    Stop using FormControllerProvider in ViewModels to avoidn potential memory leaks

--- collect_app/src/main/java/org/odk/collect/android/activities/FormEntryActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/FormEntryActivity.java
@@ -429,7 +429,7 @@ public class FormEntryActivity extends CollectAbstractActivity implements Animat
         });
 
         formEntryViewModel = ViewModelProviders
-                .of(this, new FormEntryViewModel.Factory(this::getFormController, analytics))
+                .of(this, new FormEntryViewModel.Factory(analytics))
                 .get(FormEntryViewModel.class);
 
         formEntryViewModel.getUpdates().observe(this, index -> {
@@ -439,7 +439,7 @@ public class FormEntryActivity extends CollectAbstractActivity implements Animat
         });
 
         formSaveViewModel = ViewModelProviders
-                .of(this, new FormSaveViewModel.Factory(this::getFormController))
+                .of(this, new FormSaveViewModel.Factory())
                 .get(FormSaveViewModel.class);
 
     }
@@ -499,7 +499,7 @@ public class FormEntryActivity extends CollectAbstractActivity implements Animat
             if (!newForm) {
                 if (getFormController() != null) {
                     FormController formController = getFormController();
-                    identityPromptViewModel.setAuditEventLogger(formController.getAuditEventLogger());
+                    formControllerAvailable(formController);
                     refreshCurrentView();
                 } else {
                     Timber.w("Reloading form and restoring state.");
@@ -2374,7 +2374,7 @@ public class FormEntryActivity extends CollectAbstractActivity implements Animat
 
                 if (formController.getInstanceFile() == null) {
                     createInstanceDirectory(formController);
-                    identityPromptViewModel.setAuditEventLogger(formController.getAuditEventLogger());
+                    formControllerAvailable(formController);
 
                     identityPromptViewModel.requiresIdentityToContinue().observe(this, requiresIdentity -> {
                         if (!requiresIdentity) {
@@ -2390,7 +2390,7 @@ public class FormEntryActivity extends CollectAbstractActivity implements Animat
                         // we've just loaded a saved form, so start in the hierarchy view
                         String formMode = reqIntent.getStringExtra(ApplicationConstants.BundleKeys.FORM_MODE);
                         if (formMode == null || ApplicationConstants.FormModes.EDIT_SAVED.equalsIgnoreCase(formMode)) {
-                            identityPromptViewModel.setAuditEventLogger(formController.getAuditEventLogger());
+                            formControllerAvailable(formController);
 
                             identityPromptViewModel.requiresIdentityToContinue().observe(this, requiresIdentity -> {
                                 if (!requiresIdentity) {
@@ -2422,7 +2422,7 @@ public class FormEntryActivity extends CollectAbstractActivity implements Animat
                             finish();
                         }
                     } else {
-                        identityPromptViewModel.setAuditEventLogger(formController.getAuditEventLogger());
+                        formControllerAvailable(formController);
 
                         identityPromptViewModel.requiresIdentityToContinue().observe(this, requiresIdentity -> {
                             if (!requiresIdentity) {
@@ -2439,6 +2439,12 @@ public class FormEntryActivity extends CollectAbstractActivity implements Animat
             showLongToast(R.string.loading_form_failed);
             finish();
         }
+    }
+
+    private void formControllerAvailable(FormController formController) {
+        identityPromptViewModel.formLoaded(formController.getAuditEventLogger());
+        formEntryViewModel.formLoaded(formController);
+        formSaveViewModel.formLoaded(formController);
     }
 
     private void startFormEntry(FormController formController, String warningMsg) {
